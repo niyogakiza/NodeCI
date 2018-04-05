1ceb897b5f20dadd4690b03daf237495db309a56
Aimable <niyoai@yahoo.fr>
2018-04-05 17:06:41 +0100

    working on aws and Mlab database

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://aimable:password@ds261128.mlab.com:61128/blog_dev',
   cookieKey: '123123123',
-    redisUrl:'redis://127.0.0.1:6379'
+    redisUrl:'redis://127.0.0.1:6379',
+    accessKeyId: 'AKIAIKK4PFODBKV4VY2Q',
+    secretAccessKey:'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'
 };
